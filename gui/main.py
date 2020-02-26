1ebd2b92bb97196b04f0bbb09d60c3faa36c20f8
Joe Jacobs <joej@ignw.io>
2020-02-26 10:30:52 -0800

    taking UI output and dumping to yaml file

--- gui/main.py
+++ gui/main.py
@@ -9,30 +9,37 @@ def pre():
 
 
 def main(**kwargs):
+    print(kwargs)
     repo_name = 'dc_2020_aci_appliance_server'
     now = datetime.now()
     str_now = now.strftime("%Y%m%d-%H%M%S")
 
-    templateLoader = FileSystemLoader(searchpath=f'./repos/{repo_name}/gui')
-    templateEnv = Environment(loader=templateLoader)
-    template = templateEnv.get_template('terraform.j2')
+    # templateLoader = FileSystemLoader(searchpath=f'./repos/{repo_name}/gui')
+    # templateEnv = Environment(loader=templateLoader)
+    # template = templateEnv.get_template('terraform.j2')
 
-    description = kwargs['description']
-    ip_address = kwargs['ip_address']
-    ip_octects = ip_address.split('.')
+    # description = kwargs['description']
+    # ip_address = kwargs['ip_address']
+    # ip_octects = ip_address.split('.')
+    # name = kwargs['name']
+    # name_ip_w_underscores = f"{name}_{ip_octects[0]}_{ip_octects[1]}_{ip_octects[2]}"
+    # scope = kwargs['routing']
+    # new_branch = f'{name_ip_w_underscores}_{str_now}'
+    # tf_file_name = f'network_{ name }'
+
+    # terraform_file = template.render(
+    #     name_ip_w_underscores=name_ip_w_underscores,
+    #     name=name,
+    #     ip_address=ip_address,
+    #     description=description,
+    #     scope=scope
+    # )
+
     name = kwargs['name']
-    name_ip_w_underscores = f"{name}_{ip_octects[0]}_{ip_octects[1]}_{ip_octects[2]}"
-    scope = kwargs['routing']
-    new_branch = f'{name_ip_w_underscores}_{str_now}'
-    tf_file_name = f'network_{ name }'
+    new_branch = f"{kwargs['name']}_{str_now}"
+    file_name = f'ansible_vars_{name}'
 
-    terraform_file = template.render(
-        name_ip_w_underscores=name_ip_w_underscores,
-        name=name,
-        ip_address=ip_address,
-        description=description,
-        scope=scope
-    )
+    output_data = {k:v for k,v in kwargs.items() if "github" not in k}
 
     s = SourceControlMgmt(
         username=kwargs['github_username'],
@@ -47,14 +54,14 @@ def main(**kwargs):
         print('creds validated')
         s.clone_private_repo("/tmp")
         s.create_new_branch_in_repo(new_branch)
-        s.write_data_to_file_in_repo(terraform_file, file_path='', file_name=tf_file_name)
+        s.write_data_to_file_in_repo(output_data, file_path='', file_name=file_name, as_yaml=True)
         s.push_data_to_remote_repo()
         s.delete_local_copy_of_repo()
         s.get_all_current_branches()
         pr_results = s.create_git_hub_pull_request(
             destination_branch="master",
             source_branch=new_branch,
-            title=f"Pull Request {name_ip_w_underscores}",
+            title=f"Pull Request {file_name}",
             body="")
         return pr_results
     else:
